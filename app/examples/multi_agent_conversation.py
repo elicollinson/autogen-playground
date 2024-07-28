dea2e3d1856983268271523a205b1361c245d0d8
Eli Collinson <eli@elicollinson.com>
2024-07-27 21:04:18 -0400

    initial commit

---
+++ app/examples/multi_agent_conversation.py
@@ -0,0 +1,20 @@
+import os
+
+from autogen import ConversableAgent
+
+def multi_agent_example():
+    cathy = ConversableAgent(
+        "cathy",
+        system_message="Your name is Cathy and you are a part of a duo of comedians.",
+        llm_config={"config_list": [{"model": "gpt-4o-mini", "temperature": 0.9, "api_key": os.environ.get("OPENAI_API_KEY")}]},
+        human_input_mode="NEVER",  # Never ask for human input.
+    )
+
+    joe = ConversableAgent(
+        "joe",
+        system_message="Your name is Joe and you are a part of a duo of comedians.",
+        llm_config={"config_list": [{"model": "gpt-4o-mini", "temperature": 0.7, "api_key": os.environ.get("OPENAI_API_KEY")}]},
+        human_input_mode="NEVER",  # Never ask for human input.
+    )
+    
+    result = joe.initiate_chat(cathy, message="Cathy, tell me a joke.", max_turns=2)
